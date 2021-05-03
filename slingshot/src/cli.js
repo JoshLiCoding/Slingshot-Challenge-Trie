272cff168c0d671d86cb41b789f3a130b216862e
JoshLiCoding <43073270+JoshLiCoding@users.noreply.github.com>
2021-05-02 23:30:39 -0400

    changed files

--- slingshot/src/cli.js
+++ slingshot/src/cli.js
@@ -1,50 +1,46 @@
-//https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
-//https://www.youtube.com/watch?v=AXjmTQ8LEoI
+//inspired by https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
 import arg from 'arg';
 import { add, search, deleteval, suggest, display } from './main';
 
 function parseArgumentsIntoOptions(rawArgs) {
-  const args = arg(
-    {
-      '--add': Boolean,
-      '--delete': Boolean,
-      '--search': Boolean,
-      '--suggest': Boolean,
-      '--display': Boolean,
-    },
-    {
-      argv: rawArgs.slice(2),
-    }
-  );
-  return {
-    var: args._[0],
-    add: args['--add'] || false,
-    delete: args['--delete'] || false,
-    search: args['--search'] || false,
-    suggest: args['--suggest'] || false,
-    display: args['--display'] || false,
-  };
+    const args = arg(
+        {
+            '--add': Boolean,
+            '--delete': Boolean,
+            '--search': Boolean,
+            '--suggest': Boolean,
+            '--display': Boolean,
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+    return {
+        var: args._[0],
+        add: args['--add'] || false,
+        delete: args['--delete'] || false,
+        search: args['--search'] || false,
+        suggest: args['--suggest'] || false,
+        display: args['--display'] || false,
+    };
 }
 
-/*
-https://stackoverflow.com/questions/19132867/adding-firebase-data-dots-and-forward-slashes
-*/
 export function cli(args) {
-  let options = parseArgumentsIntoOptions(args);
-  if (options['add']) {
-    //console.log(options['var']);
-    add(options['var']);
-  }
-  else if (options['delete']) {
-    deleteval(options['var']);
-  }
-  else if (options['search']) {
-    search(options['var']);
-  }
-  else if (options['suggest']) {
-    suggest(options['var']);
-  }
-  else if (options['display']) {
-    display();
-  }
+    let options = parseArgumentsIntoOptions(args);
+    if (options['add']) {
+        //console.log(options['var']);
+        add(options['var']);
+    }
+    else if (options['delete']) {
+        deleteval(options['var']);
+    }
+    else if (options['search']) {
+        search(options['var']);
+    }
+    else if (options['suggest']) {
+        suggest(options['var']);
+    }
+    else if (options['display']) {
+        display();
+    }
 }
